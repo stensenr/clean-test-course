b354440a9deac71d4a9df70f6c15cafa98498d0b
Rune Stensen <rune@moonstep.no>
2026-05-25 21:53:06 +0300

    Raise default delivery fee and update delivery tests

--- backend/hangry_api/tests/test_DeliveryCost.py
+++ backend/hangry_api/tests/test_DeliveryCost.py
@@ -1,12 +1,17 @@
 from api.controllers import Delivery
 from django_mock_queries.query import MockSet, MockModel
+import pytest
+
+
+def build_order(*quantities):
+  order = MockSet()
+  for quantity in quantities:
+    order.add(MockModel(quantity=quantity))
+  return order
 
 def test_LotsOfItems():
   #Arrange
-  order = MockSet()
-  order.add(MockModel(quantity=5))
-  order.add(MockModel(quantity=5))
-  order.add(MockModel(quantity=5))
+  order = build_order(5, 5, 5)
   delivery_distance = 6
   #Act
   cost = Delivery.calculate(order,delivery_distance)
@@ -15,10 +20,7 @@ def test_LotsOfItems():
 
 def test_MiddleOfTheRoadItems():
   #Arrange
-  order = MockSet()
-  order.add(MockModel(quantity=2))
-  order.add(MockModel(quantity=2))
-  order.add(MockModel(quantity=2))
+  order = build_order(2, 2, 2)
   delivery_distance = 4
   #Act
   cost = Delivery.calculate(order,delivery_distance)
@@ -27,9 +29,92 @@ def test_MiddleOfTheRoadItems():
 
 def test_LittleItems():
   #Arrange
-  # TODO: Arrange the items to run the test
+  order = build_order(1, 2)
+  delivery_distance = 2
   #Act
-  # TODO: Call the function that will be tested
+  cost = Delivery.calculate(order,delivery_distance)
   #Assert
-  # TODO: replace the pass with an assert to test the value returned.
-  pass
+  assert cost == 3.5
+
+def test_EmptyOrderGetsMinimumDeliveryCost():
+  #Arrange
+  order = build_order()
+  delivery_distance = 10
+  #Act
+  cost = Delivery.calculate(order,delivery_distance)
+  #Assert
+  assert cost == 3.5
+
+def test_ExactlyFiveItemsAndThreeMilesGetsMinimumDeliveryCost():
+  #Arrange
+  order = build_order(2, 3)
+  delivery_distance = 3
+  #Act
+  cost = Delivery.calculate(order,delivery_distance)
+  #Assert
+  assert cost == 3.5
+
+def test_ExactlyTenItemsAndFiveMilesGetsMiddleDeliveryCost():
+  #Arrange
+  order = build_order(5, 5)
+  delivery_distance = 5
+  #Act
+  cost = Delivery.calculate(order,delivery_distance)
+  #Assert
+  assert cost == 5
+
+def test_MoreThanTenItemsButFiveMilesGetsMiddleDeliveryCost():
+  #Arrange
+  order = build_order(6, 5)
+  delivery_distance = 5
+  #Act
+  cost = Delivery.calculate(order,delivery_distance)
+  #Assert
+  assert cost == 5
+
+def test_MoreThanFiveMilesButOnlyTenItemsGetsMiddleDeliveryCost():
+  #Arrange
+  order = build_order(5, 5)
+  delivery_distance = 6
+  #Act
+  cost = Delivery.calculate(order,delivery_distance)
+  #Assert
+  assert cost == 5
+
+def test_MoreThanFiveItemsButOnlyThreeMilesGetsMinimumDeliveryCost():
+  #Arrange
+  order = build_order(3, 3)
+  delivery_distance = 3
+  #Act
+  cost = Delivery.calculate(order,delivery_distance)
+  #Assert
+  assert cost == 3.5
+
+def test_NegativeDistanceGetsMinimumDeliveryCost():
+  #Arrange
+  order = build_order(20)
+  delivery_distance = -1
+  #Act
+  cost = Delivery.calculate(order,delivery_distance)
+  #Assert
+  assert cost == 3.5
+
+def test_ItemWithoutQuantityRaisesTypeError():
+  #Arrange
+  order = MockSet()
+  order.add(MockModel())
+  #Act / Assert
+  with pytest.raises(TypeError):
+    Delivery.calculate(order,1)
+
+def test_NoneOrderRaisesTypeError():
+  #Act / Assert
+  with pytest.raises(TypeError):
+    Delivery.calculate(None,1)
+
+def test_NonNumericDistanceRaisesTypeErrorWhenDistanceCompared():
+  #Arrange
+  order = build_order(6)
+  #Act / Assert
+  with pytest.raises(TypeError):
+    Delivery.calculate(order,"far")
